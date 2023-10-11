5e9426c7a6dd2d7a0c37351015c0e9f2b52c8df4
peiqianlong <15709242457@163.com>
2023-10-11 09:40:33 +0800

    123

--- QX/longfor.js
+++ QX/longfor.js
@@ -15,12 +15,6 @@ const {
   done,
 } = nobyda();
 
-const token = `8a6548d3a11b4a2a876318ff60cd4a4d`;
-const XLFDXRiskCaptchaToken = `undefined`;
-const XLFDXRiskToken = `65193a59jKtBUjaLFMMxfL2s2Lsymhv5b98ajLY1`;
-const Cookie = `SERVERID=e13b2eca6d4150ad68f7afa42be4d72b|1696758964|1696758962; _dx_app_d1a43734fc59aeae9f1562dbd70fdf54=65227cb4MRUZrjIlogdOc1aho1SvdpoczPBcg6b1; zg_d5bd8e6372844af9b43b8ce5bb74b787=%7B%22sid%22%3A%201696758963214%2C%22updated%22%3A%201696758964749%2C%22info%22%3A%201696556931207%2C%22superProperty%22%3A%20%22%7B%5C%22%E6%89%8B%E6%9C%BA%E5%8F%B7%5C%22%3A%20%5C%22157****2457%5C%22%2C%5C%22%E6%8C%87%E7%BA%B9%E8%AF%86%E5%88%AB%5C%22%3A%20%5C%2265227cb4lhRTQk32yowdrI9lclHHIkhvjQ6BCSm1%5C%22%2C%5C%22%E9%BE%99%E6%B0%91ID%5C%22%3A%20%5C%2233349690%5C%22%2C%5C%22%E7%94%A8%E6%88%B7%E7%B1%BB%E5%9E%8B%5C%22%3A%20%5C%22C1%5C%22%2C%5C%22%E6%B3%A8%E5%86%8C%E6%97%B6%E9%97%B4%5C%22%3A%20%5C%222021-02-28%2016%3A04%3A00%5C%22%2C%5C%22lmid%5C%22%3A%20%5C%2233349690%5C%22%2C%5C%22%E9%A1%B5%E9%9D%A2code%5C%22%3A%20%5C%2211111111111686241863606037740000%5C%22%2C%5C%22activity_no%5C%22%3A%20%5C%2211111111111686241863606037740000%5C%22%2C%5C%22storeName%5C%22%3A%20%5C%22%25E8%25A5%25BF%25E5%25AE%2589%25E5%25A4%25A7%25E5%2585%25B4%25E6%2598%259F%25E6%2582%25A6%25E8%258D%259F%5C%22%2C%5C%22channel%5C%22%3A%20%5C%22C2%5C%22%2C%5C%22buCode%5C%22%3A%20%5C%22C20400%5C%22%2C%5C%22ua%5C%22%3A%20%5C%22Mozilla%2F5.0%20(iPhone%3B%20CPU%20iPhone%20OS%2016_7%20like%20Mac%20OS%20X)%20AppleWebKit%2F605.1.15%20(KHTML%2C%20like%20Gecko)%20Mobile%2F15E148%20MicroMessenger%2F8.0.42(0x18002a2a)%20NetType%2FWIFI%20Language%2Fzh_CN%20miniProgram%2Fwx50282644351869da%5C%22%7D%22%2C%22platform%22%3A%20%22%7B%7D%22%2C%22utm%22%3A%20%22%7B%7D%22%2C%22referrerDomain%22%3A%20%22llt.longfor.com%22%2C%22landHref%22%3A%20%22https%3A%2F%2Flongzhu.longfor.com%2Flongball-homeh5%2F%23%2Fsign-in-prize%3FminiShare%3Dtrue%26activity_no%3D11111111111686241863606037740000%26lltsource%3Dc2lzgzh%26sessionId%3D8a6548d3a11b4a2a876318ff60cd4a4d%26openId%3DoAjtH4yj5f2jJvzWHA--ulN7NCBY%26storeName%3D%25E8%25A5%25BF%25E5%25AE%2589%25E5%25A4%25A7%25E5%2585%25B4%25E6%2598%259F%25E6%2582%25A6%25E8%258D%259F%26token%3D8a6548d3a11b4a2a876318ff60cd4a4d%26channel%3DC2%26buCode%3DC20400%22%2C%22cuid%22%3A%20%2233349690%22%2C%22zs%22%3A%200%2C%22sc%22%3A%200%2C%22firstScreen%22%3A%201696758963214%7D; _dx_uzZo5y=f31df9101411e0a5c416d662d7d6b1f6b3bbe33b8b5d5ed1ddc9d659e94d9b3ce145c631; zg_did=%7B%22did%22%3A%20%2218ad974a8c47ff-09dd71c6868d5c-24b245e-505c8-18ad974a8c5195e%22%7D; acw_tc=2760828016967589628262326e63cf36b010e0f5cfa3fcdfd1914a07c1bd50`;
-const XGAIAAPIKEY = `c06753f1-3e68-437d-b592-b94656ea5517`;
-const XLFUserToken = `8a6548d3a11b4a2a876318ff60cd4a4d`;
 // 龙珠签到
 (function Start() {
   const a = new Promise((resolve, rejects) => {
@@ -46,12 +40,6 @@ const XLFUserToken = `8a6548d3a11b4a2a876318ff60cd4a4d`;
       resolve();
     });
   });
-  // const c = new Promise((resolve, rejects) => {
-  //   LHzhuanpan(function (err, res, body) {
-  //     console.log(body + '我是LHzhuanpan');
-  //     resolve();
-  //   });
-  // });
   Promise.all([a, b]).then(() => {
     done();
   });
@@ -91,147 +79,6 @@ function LHSing(cb) {
     body: body,
   };
   post(myRequest, cb);
-}
-function LHHuaTi(cb) {
-  // 两个日期字符串
-  const dateString1 = '2023-10-5';
-  // 创建一个表示当前日期的 Date 对象
-  const currentDate = new Date();
-  const dateString2 = `${currentDate.getFullYear()}-${
-    currentDate.getMonth() + 1
-  }-${currentDate.getDate()}`;
-  // 计算两个日期相差几天
-  function DateDiff(date1, date2) {
-    // date1 和 date2 是 2016-06-18 格式
-    let strDate, oDate1, oDate2, result;
-    strDate = date1.split('-');
-    oDate1 = new Date(strDate[1] + '/' + strDate[2] + '/' + strDate[0]);
-    strDate = date2.split('-');
-    oDate2 = new Date(strDate[1] + '/' + strDate[2] + '/' + strDate[0]);
-    result = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 / 24); // 把相差的毫秒數轉換為天數
-    return result;
-  }
-  let DayTime = DateDiff(dateString1, dateString2);
-  let numid = 8153 + DayTime;
-  console.log(DayTime, numid);
-  const time = Math.round(new Date().getTime() / 1000).toString();
-  const url = `https://longzhu.longfor.com/proxy/lmarketing-task-api-prod/openapi/task/v1/information/list?task_id=${numid}`;
-  const method = `GET`;
-  const body = ``;
-  const headers = {
-    'X-LF-Channel': `C3`,
-    'User-Agent': `Mozilla/5.0 (iPhone; CPU iPhone OS 16_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.42(0x18002a2a) NetType/4G Language/zh_CN miniProgram/wx1c1c8c073e2be1fc`,
-    'X-LF-Bu-Code': `C30403`,
-    Cookie: `SERVERID=f3c7e85ec13830172979a766a029921a|${time}|${time}; zg_d5bd8e6372844af9b43b8ce5bb74b787=%7B%22sid%22%3A%201696745480962%2C%22updated%22%3A%201696747166884%2C%22info%22%3A%201696556931207%2C%22superProperty%22%3A%20%22%7B%7D%22%2C%22platform%22%3A%20%22%7B%7D%22%2C%22utm%22%3A%20%22%7B%7D%22%2C%22referrerDomain%22%3A%20%22%22%2C%22landHref%22%3A%20%22https%3A%2F%2Flongzhu.longfor.com%2Flongball-homeh5%2F%23%2Finformation%2F%3Ftask_id%3D8156%26channel%3DC3%26buCode%3DC30403%26miniShare%3Dfalse%26sessionId%3D6c6a1e9f12774593b0aafde8fc745fcf%26token%3D6c6a1e9f12774593b0aafde8fc745fcf%26GyVersion%3D4.9.0%26GyUnionid%3DoxlmWwjro73zgBpjKMrNmyX4MJgY%26GyUserId%3D3252920%26GyPhone%3D15709242457%26GyLmId%3D33349690%26GyVirtualPhone%3D%26GyNewMember%3D0%22%2C%22cuid%22%3A%20%2233349690%22%2C%22zs%22%3A%200%2C%22sc%22%3A%200%2C%22firstScreen%22%3A%201696745480962%7D; zg_did=%7B%22did%22%3A%20%2218ad974a8c47ff-09dd71c6868d5c-24b245e-505c8-18ad974a8c5195e%22%7D; _dx_app_d1a43734fc59aeae9f1562dbd70fdf54=65224e72pxWGfGnzSufB38rqRBgrmKrmJbjkd0g1; _dx_uzZo5y=f31df9101411e0a5c416d662d7d6b1f6b3bbe33b8b5d5ed1ddc9d659e94d9b3ce145c631; acw_tc=2760826216967454807618376e0f1f870489907024aa0251c4ea21b84fddee`,
-    'X-GAIA-API-KEY': `caed5282-9019-418d-8854-3c34d02e0b4e`,
-    'X-LF-UserToken': `1a0587235081406c9bf67ee3b061b111`,
-  };
-  const myRequest = {
-    url: url,
-    method: method,
-    headers: headers,
-    body: body,
-  };
-  get(myRequest, function (err, res, body) {
-    let a = JSON.parse(body);
-    if (a.code == '0000') {
-      const url1 = `https://longzhu.longfor.com/proxy/lmarketing-task-api-prod/openapi/task/v1/information/user`;
-      const method1 = `POST`;
-      const body1 = {
-        token: '8fe450bf5a2a4384a188177d4e7d901a',
-        channel: 'C3',
-        bu_code: 'C30403',
-        task_id: numid,
-        item_id: a.data.information[0].item_id,
-        item_content: '{"user_answer":[0,1,0]}',
-      };
-      const headers1 = {
-        Host: `longzhu.longfor.com`,
-        Origin: `https://longzhu.longfor.com`,
-        'X-LF-DXRisk-Token': `651a7e03MkfzK7b6bxlegAd6lchZxVrGJqi6OlN1`,
-        'Content-Type': `application/json;charset=UTF-8`,
-        'X-LF-DXRisk-Source': `5`,
-        Referer: `https://longzhu.longfor.com/longball-homeh5/`,
-        'X-LF-DXRisk-Captcha-Token': ``,
-        'X-LF-Bu-Code': `C30403`,
-        'EagleEye-TraceID': `a5ed03a265cb44e68c7a53aa0f9c1738`,
-        'Sec-Fetch-Mode': `cors`,
-        'X-LF-Channel': `C3`,
-        'User-Agent': `Mozilla/5.0 (iPhone; CPU iPhone OS 16_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.42(0x18002a2a) NetType/4G Language/zh_CN miniProgram/wx1c1c8c073e2be1fc`,
-        'X-LF-Bu-Code': `C30403`,
-        Cookie: `SERVERID=f3c7e85ec13830172979a766a029921a|${time}|${time}; zg_d5bd8e6372844af9b43b8ce5bb74b787=%7B%22sid%22%3A%201696745480962%2C%22updated%22%3A%201696747166884%2C%22info%22%3A%201696556931207%2C%22superProperty%22%3A%20%22%7B%7D%22%2C%22platform%22%3A%20%22%7B%7D%22%2C%22utm%22%3A%20%22%7B%7D%22%2C%22referrerDomain%22%3A%20%22%22%2C%22landHref%22%3A%20%22https%3A%2F%2Flongzhu.longfor.com%2Flongball-homeh5%2F%23%2Finformation%2F%3Ftask_id%3D8156%26channel%3DC3%26buCode%3DC30403%26miniShare%3Dfalse%26sessionId%3D6c6a1e9f12774593b0aafde8fc745fcf%26token%3D6c6a1e9f12774593b0aafde8fc745fcf%26GyVersion%3D4.9.0%26GyUnionid%3DoxlmWwjro73zgBpjKMrNmyX4MJgY%26GyUserId%3D3252920%26GyPhone%3D15709242457%26GyLmId%3D33349690%26GyVirtualPhone%3D%26GyNewMember%3D0%22%2C%22cuid%22%3A%20%2233349690%22%2C%22zs%22%3A%200%2C%22sc%22%3A%200%2C%22firstScreen%22%3A%201696745480962%7D; zg_did=%7B%22did%22%3A%20%2218ad974a8c47ff-09dd71c6868d5c-24b245e-505c8-18ad974a8c5195e%22%7D; _dx_app_d1a43734fc59aeae9f1562dbd70fdf54=65224e72pxWGfGnzSufB38rqRBgrmKrmJbjkd0g1; _dx_uzZo5y=f31df9101411e0a5c416d662d7d6b1f6b3bbe33b8b5d5ed1ddc9d659e94d9b3ce145c631; acw_tc=2760826216967454807618376e0f1f870489907024aa0251c4ea21b84fddee`,
-        'X-GAIA-API-KEY': `caed5282-9019-418d-8854-3c34d02e0b4e`,
-        'X-LF-UserToken': `1a0587235081406c9bf67ee3b061b111`,
-      };
-      const myRequest1 = {
-        url: url1,
-        method: method1,
-        headers: headers1,
-        body: JSON.stringify(body1),
-      };
-      post(myRequest1, cb);
-    } else {
-      notify('龙珠话题', '', '获取话题失败' + body);
-      done();
-    }
-  });
-}
-
-function LHzhuanpan(cb) {
-  const url = `https://openapi.longfor.com/llt-gateway-prod/api/v1/activity/auth/lottery/sign`;
-  const method = `OPTIONS`;
-  const headers = {
-    'Access-Control-Request-Method': `POST`,
-    Origin: `https://llt.longfor.com`,
-    'User-Agent': `Mozilla/5.0 (iPhone; CPU iPhone OS 16_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.42(0x18002a2a) NetType/4G Language/zh_CN miniProgram/wx50282644351869da`,
-    'Access-Control-Request-Headers': `authtoken,bucode,channel,content-type,x-gaia-api-key,x-lf-dxrisk-source,x-lf-dxrisk-token`,
-    'Sec-Fetch-Mode': `cors`,
-    Referer: `https://llt.longfor.com/`,
-    Host: `openapi.longfor.com`,
-    'Accept-Language': `zh-CN,zh-Hans;q=0.9`,
-    Accept: `*/*`,
-  };
-  const body = ``;
-  const myRequest = {
-    url: url,
-    method: method,
-    headers: headers,
-    body: body,
-  };
-  get(myRequest, function () {
-    const url1 = `https://openapi.longfor.com/llt-gateway-prod/api/v1/activity/auth/lottery/sign`;
-    const method1 = `POST`;
-    const headers1 = {
-      'Accept-Encoding': `gzip, deflate, br`,
-      bucode: `C20400`,
-      Host: `openapi.longfor.com`,
-      Origin: `https://llt.longfor.com`,
-      'Sec-Fetch-Dest': `empty`,
-      Connection: `keep-alive`,
-      channel: `C2`,
-      'Sec-Fetch-Site': `same-site`,
-      'User-Agent': `Mozilla/5.0 (iPhone; CPU iPhone OS 16_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.42(0x18002a2a) NetType/4G Language/zh_CN miniProgram/wx50282644351869da`,
-      'Content-Type': `application/json`,
-      Referer: `https://llt.longfor.com/`,
-      'X-LF-DXRisk-Source': `5`,
-      'Accept-Language': `zh-CN,zh-Hans;q=0.9`,
-      Accept: `application/json, text/plain, */*`,
-      authtoken: `36c59a03d09143fc9cab7fecee53a1fb`,
-      'Sec-Fetch-Mode': `cors`,
-      'X-LF-DXRisk-Token': XLFDXRiskToken,
-      Cookie: Cookie,
-      'x-gaia-api-key': XGAIAAPIKEY,
-      'X-LF-UserToken': XLFUserToken,
-    };
-    const body1 = `{"component_no":"CE16H35W57Q2FAPV","activity_no":"AP232091D6WUVCH1"}`;
-    const myRequest1 = {
-      url: url1,
-      method: method1,
-      headers: headers1,
-      body: body1,
-    };
-    post(myRequest1, cb);
-  });
 }
 
 function nobyda() {
